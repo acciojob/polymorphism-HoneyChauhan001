c366bd99d999923d4c1faa8f57966db89c213ba3
Honey Chauhan <hc924508@gmail.com>
2023-04-13 20:21:52 +0530

    polymorphism using Overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
+//public class Product{
+//    public int product(int x, int y){
+//
+//    }
+//}
 public class Main {
+
+    static class Product{
+        public int product(int x, int y){
+            return x+y;
+        }
+
+        public int product(int x, int y, int z){
+            return x+y+z;
+        }
+
+        public double product(double x, double y){
+            return x+y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(10.0, 20.0);
+
+
+    }
 
 }
